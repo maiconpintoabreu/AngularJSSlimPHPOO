50b45407bd7829458457f01fb402dfaed8417a6b
Maicon <maiconspas@gmail.com>
2015-10-19 18:23:41 +0100

    Bootstrap, DataBase, Select(AngularJS)

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
     <title>Index</title>
 
     <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 </head>
 
@@ -20,6 +23,20 @@
 
             <h2>Locations</h2>
 
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <select class="form-control">
+                  <option>Countries</option>
+                  <option ng-repeat="country in countries" value="{{country.id}}">{{country.name}}</option>
+                </select>
+            </div>
+            <div class="col-md-6">
+                <select class="form-control">
+                  <option>Cities</option>
+                  <option ng-repeat="city in cities" value="{{city.id}}">{{city.name}}</option>
+                </select>
+            </div>
         </div>
 
         <div class="hs-location" id="hs-location">
